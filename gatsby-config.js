580edd264d731aa89f5be7eede90459fe4473414
Ryan Ponce <ryan@ponce.la>
2019-11-20 21:09:04 -0800

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `A Ponce Thanksgiving`,
+        short_name: `Thanksgiving`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#381d12`,
+        theme_color: `#381d12`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`
       }
     }
   ]
